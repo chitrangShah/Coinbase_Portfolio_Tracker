c2b4b65c6a0a185bbee41ffff650ffc52b29efe3
chitrangShah <clshah2@gmail.com>
2021-02-21 00:12:57 -0500

    updated project structure and few base classes

---
+++ Api/Config/AppConfig.cs
@@ -0,0 +1,23 @@
+namespace Coinbase_Portfolio_Tracker.Api.Config
+{
+    public class AppConfig
+    {
+        public Coinbase Coinbase { get; set; }
+        public Google GoogleSpreadsheet { get; set; }
+    }
+
+    public abstract class Coinbase
+    {
+        public string ApiKey { get; set; }
+    }
+
+    public abstract class Google
+    {
+        public string Type { get; set; }
+        public string ProjectId { get; set; }
+        public string PrivateKeyId { get; set; }
+        public string PrivateKey { get; set; }
+        public string ClientEmail { get; set; }
+        public string ClientId { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Coinbase_Portfolio_Tracker.Api.Config;
+using Microsoft.Extensions.Configuration;
 
 namespace Coinbase_Portfolio_Tracker
 {
@@ -6,7 +8,26 @@ namespace Coinbase_Portfolio_Tracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = InitOptions<AppConfig>();
+            
+        }
+
+        private static T InitOptions<T>()
+            where T : new()
+        {
+            var config = InitConfig();
+            return config.Get<T>();
+        }
+        
+        private static IConfigurationRoot InitConfig()
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env}.json", true, true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
         }
     }
 }
